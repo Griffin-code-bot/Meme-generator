97dde5b6bbdb979245d857aa68c0ffc73cfa1805
Shubham Gupta <80226015+Griffin-code-bot@users.noreply.github.com>
2022-05-22 02:58:44 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<html>
+    <head>
+         <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="style.css">
+        <script src="index.js"></script>
+    </head>
+    <body>
+
+    
+    <?php
+echo "qualityinternship.com";
+?>
+
+                       
+   <div class=new>
+   <div id=believe>
+       <ul>
+       <li>  <p align="left">  Signup </p>  </li>
+       <li>  <p align="left">Login </p>  </li>
+       <li>Register </li>
+    </ul>
+   </div>
+<h1>INTERN IN TECH </h1>
+
+<span class=box>
+  <h1>POST AN INTERN FOR 99$ </h1>
+  </span>
+   <p align="right">
+  <input type="button" value="CREATE" />
+   <input type="button" value="POST" />
+</p>  
+                      
+<div>
+  <h3>SOFTWARE DEVELOPER INTERN </h3>
+<h4>FRONT END INTERN</h4>
+<h4>BACK END INTERN</h4>
+
+  <br>
+  <br>
+ <a style="text-decoration: none;white-space:nowrap;" class="my-btn my-btn-primary" href="https://buy.stripe.com/8wMcNvgSZ2Hm4o0aEE"
+    target=”_blank”>
+Pay $199 via card
+</a>
+
+  <br>  <br>
+  <br>
+<form action="demo.php" method="post">
+    <input type=email placeholder="xyz@gmail.com" name="email" /> 
+    <br>
+    <br>
+    <button>Submit</button>
+    <br>
+    <br>
+    SUBSCRIBE FOR EARLY ACCESS
+    <br>
+    <br>
+
+</form>
+    Your email address is: <?php echo $_POST["email"]; ?>
+
+    <!-- <?php include_once 'a.php' ?> -->
+    <?php include_once 'b.php' ?>
+
+    <?php include_once 'signup.php' ?>
+
+
+    
+    </body>
+</html>
